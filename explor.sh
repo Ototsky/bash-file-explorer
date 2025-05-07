cab488947b218f5c3efa9791e84127fe2328a66c
Ototsky <of9494of9494@gmail.com>
2025-05-07 20:56:57 +0300

    adding txt char count

--- explor.sh
+++ explor.sh
@@ -17,3 +17,20 @@ do
         echo "📁 $content ------------ $(du -sh "$content" | awk '{printf $1}')"
     fi
 done
+
+flag=1
+
+while [ "$flag" -eq 1 ]
+do
+echo ""
+read -p "Enter a line of text(Press enter without writing to exit):" text
+
+if [[ -n "$text" ]]
+then
+    echo "Characters count: $(echo -n "$text" | wc -m) characters"
+else
+    echo "Exiting the Explorer. GoodBye!"
+    flag=0
+fi
+
+done
